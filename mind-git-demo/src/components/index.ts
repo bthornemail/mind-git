9e30c32b8477fe78e9b171cf47324a379c735195
Brian Thorne <bthornemail@gmail.com>
2026-03-05 01:38:44 -0800

    chore: snapshot local state (commit-all-030526)

--- mind-git-demo/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Canvas3D } from './Canvas3D';
-export { Node3D } from './Node3D';
-export { Edge3D } from './Edge3D';
-export { CompilerPanel } from './CompilerPanel';
-export { NodeGeometry, GLTFNode } from './NodeGeometry';
-export { ModelGallery } from './ModelGallery';
-export { ModelSelector } from './ModelSelector';
